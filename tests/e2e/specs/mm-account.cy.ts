62f7f74dab07e9285fdba6f97695ce17c6adee22
cianekjr <cianekjr@gmail.com>
2023-01-18 12:12:23 +0100

    Use xvfb

--- tests/e2e/specs/mm-account.cy.ts
+++ tests/e2e/specs/mm-account.cy.ts
@@ -1,73 +1,52 @@
-// import { v4 as uuidV4 } from "uuid";
+import { v4 as uuidV4 } from "uuid";
 
-// import MyProfilePage from "../pages/my-profile-page";
-// import Auth from "../pages/auth";
-// import HomePage from "../pages/home-page";
-// import { generateNewWallet } from "../helpers/wallet";
-// import Global from "../pages/global";
+import Auth from "../pages/auth";
+import HomePage from "../pages/home-page";
+import { generateNewWallet } from "../helpers/wallet";
+import Global from "../pages/global";
 
-// const myProfile = new MyProfilePage();
+const home = new HomePage();
+const auth = new Auth();
+const global = new Global();
 
-// const home = new HomePage();
-// const auth = new Auth();
-// const global = new Global();
+const config = {
+  nickname: "uniqly",
+  email: `test+${uuidV4()}@gmail.com`,
+};
 
-// const config = {
-//   nickname: "uniqly",
-//   email: `test+${uuidV4()}@gmail.com`,
-// };
+const login = (name: string, callback: () => void) => {
+  cy.session(name, () => {
+    home.visit();
+    auth.pressLoginButton();
+    auth.pressConnectMetamask();
 
-// const login = (name: string, callback: () => void) => {
-//   cy.session(name, () => {
-//     home.visit();
-//     auth.pressLoginButton();
-//     auth.pressConnectMetamask();
+    callback();
 
-//     callback();
+    auth.isLoggedIn();
+  });
+};
 
-//     auth.isLoggedIn();
-//   });
-// };
+describe("MM account", () => {
+  before(() => {
+    const wallet = generateNewWallet();
+    cy.importMetamaskAccount(wallet.privateKey);
+  });
 
-// describe("MM account", () => {
-//   before(() => {
-//     const wallet = generateNewWallet();
-//     cy.importMetamaskAccount(wallet.privateKey);
-//   });
+  after(() => {
+    cy.switchMetamaskAccount("Account 1");
+  });
 
-//   after(() => {
-//     cy.switchMetamaskAccount("Account 1");
-//   });
+  it(`should sign up user`, () => {
+    login("metamask-session-register", () => {
+      global.acceptMetamaskAccessRequest(true);
+    });
+  });
 
-//   it(`should sign up user`, () => {
-//     login("metamask-session-register", () => {
-//       global.acceptMetamaskAccessRequest(true);
-//     });
-//   });
+  it(`should log in user`, () => {
+    cy.clearCookies();
 
-//   it(`should log in user`, () => {
-//     cy.clearCookies();
-
-//     login("metamask-session-login", () => {
-//       cy.confirmMetamaskSignatureRequest();
-//     });
-//   });
-
-//   it(`should update user profile`, () => {
-//     login("metamask-session-login", () => {
-//       cy.confirmMetamaskSignatureRequest();
-//     });
-
-//     myProfile.visit();
-
-//     myProfile.fillNickname(config.nickname);
-//     myProfile.fillEmail(config.email);
-//     myProfile.submitMyProfileUpdate();
-
-//     cy.waitForResources();
-//     cy.reload();
-
-//     myProfile.checkNickname(config.nickname);
-//     myProfile.checkEmail(config.email);
-//   });
-// });
+    login("metamask-session-login", () => {
+      cy.confirmMetamaskSignatureRequest();
+    });
+  });
+});
